65680645594699cedcabf57d3da21f560f514b3a
Hang Qi <qih@qihang.im>
2013-04-08 13:47:58 -0700

    Deploy the top-level script.

--- ner/ner_deploy.sh
+++ ner/ner_deploy.sh
@@ -3,12 +3,13 @@
 # Deploy ner module (tSegment.jar) to /usr/local/bin folder
 
 src_dir='/home/csa/CAS2/Mingjie/tSegment/'
-target_dir='/usr/local/bin/ner/'
+target_dir='/usr/local/bin/tSegment/'
 
 echo Deploying ner...
+cp generate-ner.sh /usr/local/bin
 
 cd ${src_dir}
 cp tSegment.jar ${target_dir}
 cp -r lib ${target_dir}
-
+cp -r Data ${target_dir}
 echo Done.
