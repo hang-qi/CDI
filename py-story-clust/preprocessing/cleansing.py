bc669b4c116d62f4c9424aa2c8ebd05ca8f4094e
Weixin Li <lexiwzx@gmail.com>
2013-05-10 18:19:25 -0700

    Removing Stopwords Function

--- py-story-clust/preprocessing/cleansing.py
+++ py-story-clust/preprocessing/cleansing.py
@@ -3,8 +3,14 @@ from nltk.corpus import wordnet as wn
 
 def remove_stopwords(words):
     """Return a list of words with stopwords removed."""
-    # TODO: to be implemented.
-    return
+    stopwords_file = open('stopwords.txt', 'r')
+    data = stopwords_file.read()
+    stopwords = data.split(', ')
+    new_words = []
+    for w in words:
+        if w not in stopwords:
+            new_words.append(w)
+    return new_words
 
 
 def morphy(word):
@@ -14,9 +20,7 @@ def morphy(word):
     result_set.append(wn.morphy(word, wn.VERB))
     result_set.append(wn.morphy(word, wn.ADJ))
     result_set.append(wn.morphy(word, wn.ADV))
-
     result = None
-
     for word in result_set:
         if word is not None:
             if result is None:
@@ -32,6 +36,9 @@ def main():
     word_morphied = morphy(word)
     print('The original word is ', word,
           ' and the morphied word is ', word_morphied)
+    words = ['a', 'news', 'turn', 'walk', 'talk']
+    words_rm_stopwords = remove_stopwords(words)
+    print('Words: ', words, ' and the words after removing the stopwords: ', words_rm_stopwords)
 
 
 if __name__ == '__main__':
