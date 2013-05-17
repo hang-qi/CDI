a24bf77e73cbf8f0a41ac7eb978d662056762f41
Hang Qi <qih@qihang.im>
2013-05-17 15:59:14 -0700

    Create a reusable function to generate vocab file.

--- py-story-clust/tools/gen_new_vocab.py
+++ py-story-clust/tools/gen_new_vocab.py
@@ -4,21 +4,33 @@ sys.path.append('..')
 from vocabulary import vocabulary
 
 
-def main():
-    voc_np_old = vocabulary.Vocabulary()
-    voc_np_old.load('../mat/vp_before_calculate_matrix.voc')
-    print('Before delete: {0}'.format(voc_np_old.size()))
+def delete_words_in_vocab(old_vocab, to_delete):
+    voc_old = vocabulary.Vocabulary()
+    voc_old.load(old_vocab)
+    print('Before delete: {0}'.format(voc_old.size()))
 
-    voc_np_to_delete = vocabulary.Vocabulary()
-    voc_np_to_delete.load('../mat/vp_to_delete.voc')
-    print('To delete: {0}'.format(voc_np_to_delete.size()))
+    voc_to_delete = vocabulary.Vocabulary()
+    voc_to_delete.load(to_delete)
+    print('To delete: {0}'.format(voc_to_delete.size()))
 
-    new_word_list = [w for w in voc_np_old.word_list if not w in voc_np_to_delete.word_list]
-    voc_np_new = vocabulary.Vocabulary()
-    voc_np_new.build_by_list(new_word_list)
+    new_word_list = [w for w in voc_old.word_list if not w in voc_to_delete.word_list]
+    voc_new = vocabulary.Vocabulary()
+    voc_new.build_by_list(new_word_list)
     print('After deletion: {0}'.format(len(new_word_list)))
-    voc_np_new.save('../mat/vp.voc')
-    return
+
+    return voc_new
+
+
+def main():
+    # convert np vocabulary
+    voc_new = delete_words_in_vocab(
+        '../mat/np_before_calculate_matrix.voc', '../mat/np_to_delete.voc')
+    voc_new.save('../mat/np.voc')
+
+    # convert vp vocabulary
+    voc_new = delete_words_in_vocab(
+        '../mat/vp_before_calculate_matrix.voc', '../mat/vp_to_delete.voc')
+    voc_new.save('../mat/vp.voc')
 
 
 if __name__ == '__main__':
