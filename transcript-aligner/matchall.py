eb3a2e7a9aa01f640123744d38f3b4a2e99ac8eb
Hang Qi <qih@qihang.im>
2013-05-29 10:25:01 -0700

    Reformat to PEP8. Added two more shows to programDict.

--- transcript-aligner/matchall.py
+++ transcript-aligner/matchall.py
@@ -5,84 +5,99 @@ import datetime
 import subprocess
 
 import config
+
+
 # Tranverse all files under given dir, save all filenames in a list.
 def getHTMLFiles(directory):
     files = []
-    for dirpath, dirnames, filenames in os.walk(directory): # Walk directory tree
+    for dirpath, dirnames, filenames in os.walk(directory):  # Walk directory tree
         for filename in filenames:
             if (filename[-5:].lower() == ".html"):
                 files.append(os.path.join(dirpath, filename))
     return files
 
-# rename the html transcript by cp.
+
 def renameHTMLTranscript(oldFilename, newFilename):
+    """rename the html transcript by cp."""
     if not os.path.exists(newFilename):
-        #os.system("cp -p {src} {tgt}".format(src=oldFilename, tgt=newFilename))
-        r = subprocess.call(['cp', '-p', oldFilename, newFilename]);
+        # os.system("cp -p {src} {tgt}".format(src=oldFilename,
+        # tgt=newFilename))
+        r = subprocess.call(['cp', '-p', oldFilename, newFilename])
         print('[GOOD] HTML renamed: {0}'.format(newFilename))
     else:
         print('[GOOD] HTML exists: {0}'.format(newFilename))
-    return True;
-    
+    return True
+
+
 def matchAll(htmlTranscriptFiles):
-    
-    dt_dayStart = datetime.datetime.now();
-    num_integrated = 0;
-    num_dayintegrated = 0;
-    num_daycount = 0;
+    dt_dayStart = datetime.datetime.now()
+    num_integrated = 0
+    num_dayintegrated = 0
+    num_daycount = 0
 
     for originalHTMLTranscript in htmlTranscriptFiles:
-        
+
         # Time keeping for each day
-        dt_dayNow = datetime.datetime.now();
+        dt_dayNow = datetime.datetime.now()
         if not dt_dayStart.day == dt_dayNow.day:
             print("[TIMER] DAY - {0} of {1} transcripts integrated -- runtime {2} on {3:%Y-%m-%d}.".format(
-                num_dayintegrated, num_daycount, str(dt_dayNow - dt_dayStart), dt_dayStart));
-            num_daycount = 0;
-            dt_dayStart = dt_dayNow;
-        num_daycount += 1;
+                num_dayintegrated, num_daycount, str(dt_dayNow - dt_dayStart), dt_dayStart))
+            num_daycount = 0
+            dt_dayStart = dt_dayNow
+        num_daycount += 1
 
         print('-' * 20)
-        print('For ' + originalHTMLTranscript.replace(os.path.dirname(originalHTMLTranscript),''))
-        
+        print('For ' + originalHTMLTranscript.replace(
+            os.path.dirname(originalHTMLTranscript), ''))
+
         try:
-            matchedFilenames = matchfile.getMatchedFilenames(originalHTMLTranscript)        
-            targetHTMLFilename = matchedFilenames[0];
-            targetCaptionFilename = matchedFilenames[1];
+            matchedFilenames = matchfile.getMatchedFilenames(
+                originalHTMLTranscript)
+            targetHTMLFilename = matchedFilenames[0]
+            targetCaptionFilename = matchedFilenames[1]
 
             if (len(targetHTMLFilename) == 0):
-                continue;
+                continue
 
-            captionExist = False;
-            transcriptExist = False;
-            
+            captionExist = False
+            transcriptExist = False
+
             # check if the caption file with same name exists
             if not os.path.exists(targetCaptionFilename):
-                print('[MISSING ALERT] Caption missing at {0}'.format(targetCaptionFilename));
+                print('[MISSING ALERT] Caption missing at {0}'.format(
+                    targetCaptionFilename))
             else:
-                print('[GOOD] Caption found at {0}'.format(targetCaptionFilename))
-                captionExist = True;
+                print('[GOOD] Caption found at {0}'.format(
+                    targetCaptionFilename))
+                captionExist = True
 
             # rename HTML transcript file
-            transcriptExist = renameHTMLTranscript(originalHTMLTranscript, targetHTMLFilename)
+            transcriptExist = renameHTMLTranscript(
+                originalHTMLTranscript, targetHTMLFilename)
 
-            # run html parser to generate rawtxt for renamed html transcript file. 
+            # run html parser to generate rawtxt for renamed html transcript
+            # file.
             if (transcriptExist):
-                #os.system("python3 htmlparser.py {transcript}".format(transcript=targetHTMLFilename))
-                r = subprocess.call(['python3', config.ROOT_SCRIPT + 'htmlparser.py', targetHTMLFilename]);
+                # os.system("python3 htmlparser.py
+                # {transcript}".format(transcript=targetHTMLFilename))
+                r = subprocess.call(
+                    ['python3', config.ROOT_SCRIPT + 'htmlparser.py', targetHTMLFilename])
 
             # run aligner, given caption file and raw txt file
             if (captionExist and transcriptExist):
-                parsedTranscriptFilename = targetHTMLFilename.replace(".html", ".rawtxt")
-                r = subprocess.call([config.ROOT_ALIGNER + 'aligner', targetCaptionFilename, parsedTranscriptFilename]);
-                num_integrated += 1;
-                num_dayintegrated += 1;
+                parsedTranscriptFilename = targetHTMLFilename.replace(
+                    ".html", ".rawtxt")
+                r = subprocess.call(
+                    [config.ROOT_ALIGNER + 'aligner', targetCaptionFilename, parsedTranscriptFilename])
+                num_integrated += 1
+                num_dayintegrated += 1
             pass
         except:
             print("[ERROR] ", sys.exc_info()[0])
         finally:
             pass
-    return num_integrated;
+    return num_integrated
+
 
 def main():
     if (len(sys.argv) < 2):
@@ -90,25 +105,25 @@ def main():
         print("  e.g. > Python3 matchfile.py data/")
         return
 
-    dt_start = datetime.datetime.now();
+    dt_start = datetime.datetime.now()
     transcriptFolder = sys.argv[1]
     print("Processing Transcript directory:" + transcriptFolder)
     print("Traversing...")
     htmlTranscriptFiles = getHTMLFiles(transcriptFolder)
     print("Making copies...")
     num_integrated = matchAll(htmlTranscriptFiles)
-    dt_end = datetime.datetime.now();
-    print("Done.");
+    dt_end = datetime.datetime.now()
+    print("Done.")
 
-    print('=' * 20);
-    print("Time: ");
-    print("    Start Time: {0:%Y-%m-%d %H:%M}".format(dt_start));
-    print("    End Time:   {0:%Y-%m-%d %H:%M}".format(dt_end));
-    print("    Runtime:    {0}".format(str(dt_end - dt_start)));
-    print("");  
+    print('=' * 20)
+    print("Time: ")
+    print("    Start Time: {0:%Y-%m-%d %H:%M}".format(dt_start))
+    print("    End Time:   {0:%Y-%m-%d %H:%M}".format(dt_end))
+    print("    Runtime:    {0}".format(str(dt_end - dt_start)))
+    print("")
     print("[TIMER] {0} of {1} transcripts integrated -- runtime {2} started at {3:%Y-%m-%d %H:%M}.".format(
-        num_integrated, len(htmlTranscriptFiles), str(dt_end - dt_start), dt_start));
-    print('=' * 20);
+        num_integrated, len(htmlTranscriptFiles), str(dt_end - dt_start), dt_start))
+    print('=' * 20)
 
 if __name__ == '__main__':
     main()
--- transcript-aligner/matchfile.py
+++ transcript-aligner/matchfile.py
@@ -13,59 +13,66 @@ import config
 # This is the program name mapping for matching the target caption file
 #   Key: the program name appears in the head section of CNN transcript
 #   Value: the program name appears in caption filename, eg. .._US_CNN_Program_Name.txt
-# For program names that are not in this Dict, they will be converted by convertProgramName().
+# For program names that are not in this Dict, they will be converted by
+# convertProgramName().
 programDict = {
-    "ANDERSON COOPER 360 DEGREES"   : "Anderson_Cooper_360",
-    "CNN NEWSROOM"                  : "Newsroom",
-    "THE SITUATION ROOM"            : "Situation_Room",
-    "CNN LARRY KING LIVE"           : "Larry_King_Live",
-    "JOHN KING USA"                 : "John_King_USA",
-    "YOUR WORLD TODAY"              : "Your_World_Today",
-    "AMERICAN MORNING"              : "American_Morning",
-    "PIERS MORGAN TONIGHT"          : "Piers_Morgan_Tonight",
-    "ERIN BURNETT OUT FRONT"        : "Erin_Burnett_Out_Front",
-    "PAULA ZAHN NOW"                : "Paula_Zahn_Now",
-    "LOU DOBBS TONIGHT"             : "Lou_Dobbs_Tonight",
-    "IN THE ARENA"                  : "In_The_Arena"
+    "ANDERSON COOPER 360 DEGREES": "Anderson_Cooper_360",
+    "CNN NEWSROOM": "Newsroom",
+    "THE SITUATION ROOM": "Situation_Room",
+    "CNN LARRY KING LIVE": "Larry_King_Live",
+    "JOHN KING USA": "John_King_USA",
+    "YOUR WORLD TODAY": "Your_World_Today",
+    "AMERICAN MORNING": "American_Morning",
+    "PIERS MORGAN TONIGHT": "Piers_Morgan_Tonight",
+    "ERIN BURNETT OUT FRONT": "Erin_Burnett_Out_Front",
+    "PAULA ZAHN NOW": "Paula_Zahn_Now",
+    "LOU DOBBS TONIGHT": "Lou_Dobbs_Tonight",
+    "IN THE ARENA": "In_The_Arena",
+    "CNN LARRY KING LIVE": "Larry_King_Live",
+    "CNN LATE EDITION WITH WOLF BLITZER": "Late_Edition_With_Wolf_Blitzer",
+    "DL HUGHLEY BREAKS NEWS": "DL_Hughley_Breaks_The_News"
 }
 
 # Words will keep capitalized in file name, usually a acronym.
 #   e.g. we want to keep "CNN" instead of "Cnn", "USA" instead of "Usa".
 acronymList = ['CNN', 'USA']
 
+
 def convertProgramName(programName):
-    words = programName.replace('/',' ').split(' ')
+    words = programName.replace('/', ' ').split(' ')
     newName = ''
     for w in words:
         if (w == 'THE'):
             continue        # skip 'the'
-        
+
         if not w in acronymList:
-            w = w.lower();
-            w = w.replace(w[0], w[0].upper(), 1); # make the first letter capitalized
+            w = w.lower()
+            w = w.replace(w[0], w[
+                          0].upper(), 1)  # make the first letter capitalized
 
             # Get rid of punctuation
-            punctuation = False;
-            t = [];
+            punctuation = False
+            t = []
             for i in range(0, len(w)):
                 if not w[i].isalnum():
-                    punctuation = True;
+                    punctuation = True
                 elif punctuation == True and w[i].isalpha():
-                    t.append(w[i].upper());                    
-                    punctuation = False;
+                    t.append(w[i].upper())
+                    punctuation = False
                 else:
-                    t.append(w[i]);
-            w = ''.join(t);
+                    t.append(w[i])
+            w = ''.join(t)
 
-        newName += (w + '_')    
-    newName = newName.rstrip('_');              # strip the last '_' at the end
-    return newName;
+        newName += (w + '_')
+    newName = newName.rstrip('_')
+    # strip the last '_' at the end
+    return newName
 
-# Return matched caption filenames with full path
-# The return is a list with two element (transcript file, caption file)
 def getMatchedFilenames(transcriptFile, charset='ISO-8859-1'):
+    """Return matched caption filenames with full path
+    The return is a list with two element (transcript file, caption file)"""
     # read in transcriptFile
-    str_list = [];
+    str_list = []
     f = codecs.open(transcriptFile, encoding=charset)
     for line in f:
         str_list.append(line)
@@ -78,29 +85,31 @@ def getMatchedFilenames(transcriptFile, charset='ISO-8859-1'):
 
     # extract program name from head section
     head = parser.get_head()
-    programName = head.replace("(HEAD) ", "").replace('\n','')
-    
-    utc = timezone.UTC();
-    et_dt = parser.get_time();
-    utc_dt = et_dt.astimezone(utc)
-    
-    return buildFilenames(utc_dt, programName);
+    programName = head.replace("(HEAD) ", "").replace('\n', '')
 
-# Round to the nearest hour or half hour
+    utc = timezone.UTC()
+    et_dt = parser.get_time()
+    utc_dt = et_dt.astimezone(utc)
+
+    return buildFilenames(utc_dt, programName)
+
+
 def roundToHalfHour(dt):
-    margin = 5;
+    """Round to the nearest hour or half hour."""
+    margin = 5
     if (dt.minute in range(0, margin)):
         dt = dt.replace(minute=0)
-    elif (dt.minute in range(60-margin, 60)):
+    elif (dt.minute in range(60 - margin, 60)):
         dt = dt.replace(minute=0)
         dt = dt + datetime.timedelta(hours=1)
-    elif (dt.min in range(30-margin, 30+margin)):
+    elif (dt.min in range(30 - margin, 30 + margin)):
         dt = dt.replace(minute=30)
     return dt
 
-# Return matched filenames with full path
-# The return is a list with two element (transcript file, caption file)
+
 def buildFilenames(utcDatetime, program, agent='CNN', locale='US'):
+    """Return matched filenames with full path.
+    The return is a list with two element (transcript file, caption file)."""
     transcriptRoot = config.ROOT_TRANSCRIPT
     captionRoot = config.ROOT_CAPTION
 
@@ -109,27 +118,29 @@ def buildFilenames(utcDatetime, program, agent='CNN', locale='US'):
 
     if not utcRoundedDatetime == utcDatetime:
         print("[TIME ALERT] Time in transcript rounded for {0} on {1:%Y-%m-%d} from {2:%H:%M} to {3:%H:%M}".format(
-                 program, utcDatetime, utcDatetime, utcRoundedDatetime));
-    
-    programName = '';
+              program, utcDatetime, utcDatetime, utcRoundedDatetime))
+
+    programName = ''
     try:
-        programName = programDict[program];
+        programName = programDict[program]
         pass
     except:
-        programName = convertProgramName(program);
-        print('[RENAME] Program {0} renamed as {1}.'.format(program, programName));
-        #return ('','')
+        programName = convertProgramName(program)
+        print('[RENAME] Program {0} renamed as {1}.'.format(
+            program, programName))
+        # return ('','')
 
     airedDateTime = "{:%Y-%m-%d_%H%M}".format(utcRoundedDatetime)
-    filename = "{0}_{1}_{2}_{3}".format(airedDateTime, locale, agent, programName)
+    filename = "{0}_{1}_{2}_{3}".format(
+        airedDateTime, locale, agent, programName)
 
-    
     fullPathFormat = "{root}{dateDir}{file}{ext}"
-    transcriptFullPath = fullPathFormat.format(root=transcriptRoot, 
-                dateDir=dateDirectories, file=filename, ext=".html")
-    captionFullPath = fullPathFormat.format(root=captionRoot, 
-                dateDir=dateDirectories, file=filename, ext=".txt")
+    transcriptFullPath = fullPathFormat.format(root=transcriptRoot,
+                                               dateDir=dateDirectories, file=filename, ext=".html")
+    captionFullPath = fullPathFormat.format(root=captionRoot,
+                                            dateDir=dateDirectories, file=filename, ext=".txt")
     return (transcriptFullPath, captionFullPath)
+
 
 def main():
     if (len(sys.argv) < 2):
@@ -142,4 +153,4 @@ def main():
         print(getMatchedFilenames(filename))
 
 if __name__ == '__main__':
-	main()
+    main()
