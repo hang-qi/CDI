dc0b75c001d2e04bcb99333bea7518c4a15339a2
Weixin Li <lexiwzx@gmail.com>
2013-07-03 11:08:47 -0700

    Update

--- sw-clust/sw_process.py
+++ sw-clust/sw_process.py
@@ -13,6 +13,9 @@ def intermediate_callback(current_labeling):
 def SW_Process():
     [all_nodes, true_segment, class_num, np1_voc, vp_voc, np2_voc, np1_prob, vp_prob, np2_prob, class_prior_prob, transition_prob] = readingfiles.preprocessing('2008081519', 'preprocessing/model_segmenter.txt')
     print('Reading Files Finished')
+    print('Calculate Probability Given Category for Each Node')
+    all_nodes.set_nodes_prob(class_num, np1_voc, vp_voc, np2_voc, np1_prob, vp_prob, np2_prob)
+
     node_number = all_nodes.node_num
     edges = []
     for i in range(0, node_number-1):
@@ -20,6 +23,7 @@ def SW_Process():
         edges.append([i, j])
 
     stat = Statistics(all_nodes, class_num, np1_voc, vp_voc, np2_voc, np1_prob, vp_prob, np2_prob, class_prior_prob, transition_prob)
+    print('Start Sampling')
     sw.sample(node_number, edges, stat.calculate_Qe, stat.target_evaluation_func, intermediate_callback)
 
 
