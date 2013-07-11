8712e27395db4a33dbb50992380855db51d18818
Hang Qi <qih@qihang.im>
2013-07-10 18:16:50 -0700

    Change cluster representation to a list of sets. e.g [{}, {}, {}].

--- sw-clust/algorithm/sw.py
+++ sw-clust/algorithm/sw.py
@@ -68,13 +68,13 @@ Parameters:
 class SWContext(object):
     def __init__(self):
         self.iteration_counter = 0
-        self.current_labeling = []
+        self.current_clustering = []
 
     def count_iteration(self):
         self.iteration_counter += 1
 
-    def set_result(self, current_labeling):
-        self.current_labeling = current_labeling
+    def set_result(self, current_clustering):
+        self.current_clustering = current_clustering
 
 
 class _AdjacencyGraph(object):
@@ -103,28 +103,28 @@ class _AdjacencyGraph(object):
 
 class _ConvergenceMonitor(object):
     def __init__(self, monitor_func, epsilon=0, stationary_steps=500):
-        self.monitor_statistics_func = monitor_func
-        self.epsilon = epsilon
-        self.stationary_steps = stationary_steps
-        self.history = []
-        self.window = (0, 0)
+        self.__monitor_statistics_func = monitor_func
+        self.__epsilon = epsilon
+        self.__stationary_steps = stationary_steps
+        self.__history = []
+        self.__window = (0, 0)
 
     def has_converged(self, context):
-        if self.monitor_statistics_func is None:
+        if self.__monitor_statistics_func is None:
             return False
 
-        current_statistics = self.monitor_statistics_func(context.current_labeling)
+        current_statistics = self.__monitor_statistics_func(context.current_clustering)
         # record history
-        self.history.append(current_statistics)
+        self.__history.append(current_statistics)
         # adjust convergence window
-        self.window = (self.window[0], len(self.history) - 1)
+        self.__window = (self.__window[0], len(self.__history) - 1)
 
-        values_in_window = self.history[self.window[0]: self.window[1]+1]
-        while max(values_in_window) - min(values_in_window) > self.epsilon:
-            self.window = (self.window[0]+1, self.window[1])
-            values_in_window = self.history[self.window[0]: self.window[1]+1]
+        values_in_window = self.__history[self.__window[0]: self.__window[1]+1]
+        while max(values_in_window) - min(values_in_window) > self.__epsilon:
+            self.__window = (self.__window[0]+1, self.__window[1])
+            values_in_window = self.__history[self.__window[0]: self.__window[1]+1]
 
-        if self.window[1]-self.window[0] < self.stationary_steps:
+        if self.__window[1]-self.__window[0] < self.__stationary_steps:
             return False
         else:
             return True
@@ -139,9 +139,9 @@ class _SWCuts(object):
     def sample(self, adjacency_graph, edge_prob_func, target_eval_func, intermediate_callback=None, initial_labeling=None, monitor_statistics_func=None, max_labels=None):
         # Initial labeling.
         if initial_labeling is not None:
-            current_labeling = initial_labeling
+            current_clustering = initial_labeling
         else:
-            current_labeling = [0] * adjacency_graph.size
+            current_clustering = [set(range(0, adjacency_graph.size))]
 
         self.adjacency_graph = adjacency_graph
         if max_labels is not None:
@@ -154,47 +154,36 @@ class _SWCuts(object):
         self.target_eval_func = target_eval_func
         self.convergence_monitor = _ConvergenceMonitor(monitor_statistics_func)
 
-        self.context.set_result(current_labeling)
-
-        # Cache turn-on probability of each edge and stored in a adjacent list.
-        # Since the edge probability only concern the two end point of the edge,
-        # this probability will remain the same during the run.
-        #self.__cache_turn_on_probability_func(edge_prob_func)
+        self.context.set_result(current_clustering)
 
         while not self.__has_converged():
             self.context.count_iteration()
-            self.context.set_result(current_labeling)
+            self.context.set_result(current_clustering)
 
             # Determine edge status (on or off) probabilistically.
             edge_status = self.__determine_edge_status()
 
             # Form connected components over the whole space.
             connected_components = self.__form_connected_components(
-                current_labeling, edge_status)
+                current_clustering, edge_status)
 
             # Do a sweep
             #for component in connected_components:
             component = connected_components[random.randint(0, len(connected_components)-1)]
             # Flip the connect component probabilistically.
-            current_labeling = self.__flip_connected_component(
-                current_labeling, component)
-            self.context.set_result(current_labeling)
+            current_clustering = self.__flip_connected_component(
+                current_clustering, component)
+            self.context.set_result(current_clustering)
 
             # Propagate intermediate result if has callback function.
             if intermediate_callback is not None:
-                intermediate_callback(copy.copy(current_labeling), self.context)
+                intermediate_callback(copy.copy(current_clustering), self.context)
 
-        return current_labeling
+        return current_clustering
 
     def __has_converged(self):
         """Convergence Test."""
         return self.convergence_monitor.has_converged(self.context)
-
-    #def __cache_turn_on_probability_func(self, edge_prob_func):
-    #    """Store turn-on probability of each edge in a adjacent list."""
-    #    self.__edge_on_prob_cache = defaultdict(dict)
-    #    for (s, t) in self.adjacency_graph.edges:
-    #            self.__edge_on_prob_cache[s][t] = edge_prob_func(s, t, self.context)
 
     def __edge_on_probability(self, s, t):
         # Ensure s < t
@@ -217,7 +206,7 @@ class _SWCuts(object):
                 edge_status[t][s] = False
         return edge_status
 
-    def __form_connected_components(self, current_labeling, edge_status):
+    def __form_connected_components(self, current_clustering, edge_status):
         """Form connected components (CP) probabilistically.
         This function returns a list of CPs.
         Each CP is a list of vertexes. And cut_edges is a list of edges, i.e. [(s, t)]."""
@@ -225,77 +214,90 @@ class _SWCuts(object):
         visited = [False for v in range(0, size)]
 
         connected_components = []
-        for v in range(0, self.adjacency_graph.size):
-            if not visited[v]:
-                seed_vertex = v
-                # Form one CP from a random vertex
-                #seed_vertex = random.randint(0, size-1)
-                component = self.__grow_component_by_bfs(
-                    seed_vertex, current_labeling, edge_status, visited)
-                connected_components.append(component)
+        for cluster in current_clustering:
+            for v in cluster:
+                if not visited[v]:
+                    seed_vertex = v
+                    # Form one CP from a random vertex
+                    #seed_vertex = random.randint(0, size-1)
+                    component = self.__grow_component_by_bfs(
+                        seed_vertex, seed_cluster, edge_status, visited)
+                    connected_components.append(component)
+
         return connected_components
 
-    def __grow_component_by_bfs(self, start_vertex, current_labeling, edge_status, visited):
-        component = []
-        # cut_edges = []
-        original_label = current_labeling[start_vertex]
-        d = deque([start_vertex])
+    def __grow_component_by_bfs(self, seed_vertex, seed_cluster, edge_status, visited):
+        component = set()
+        d = deque([seed_vertex])
         while(len(d) != 0):
             v = d.popleft()
             if visited[v]:
                 continue
 
             visited[v] = True
-            component.append(v)
+            component.add(v)
 
             # Add all connected vertex into the queue.
             for u in self.adjacency_graph.adj_list[v]:
-                if current_labeling[u] == original_label:
+                if u in seed_cluster:
                     if edge_status[v][u]:
                         d.append(u)
-                    #else:
-                        # cut_edges.append((v, u))
-        # cut_edges = [(s, t) for (s, t) in cut_edges if not (s in component and t in component)]
         return component
 
-    def __flip_connected_component(self, current_labeling, component):
+    def __flip_connected_component(self, current_clustering, component):
         # Possible labels for for connected component:
         #   - Label of any neighbors, so that the CP will be merged into a neighbor.
         #   - New label, so that the CP will be a new cluster.
 
         # Find candidate labels from neighbors
-        candidate_labels = set()
+
+        for (cluster_index, cluster) in enumerate(current_clustering):
+            for v in cluster:
+                cluster_dict[v] = cluster_index
+
+        host_cluster_index = cluster_dict[component[0]]
+        neighbor_clusters = set()
         cut_edges_dict = defaultdict(set)
+
         for v in component:
             for u in self.adjacency_graph.adj_list[v]:
                 if u not in component:
-                    candidate_label = current_labeling[u]
-                    candidate_labels.add(candidate_label)
-                    cut_edges_dict[candidate_label].add((v, u))
-        # New label
-        for label in range(0, self.max_labels):
-            if label not in candidate_labels:
-                candidate_labels.add(label)
-                cut_edges_dict[label] = set()
-                break
+                    neighbor_cluster_index = cluster_dict[u]
+                    neighbor_clusters.add(neighbor_cluster_index)
+                    cut_edges_dict[neighbor_cluster_index].add((v, u))
 
-        # Compute posterior probability of each candidate.
-        labeling_candidates = []
+        # Generate candidates
+        # 1. Add all vertex in component to one of the neighbor
+        #    and remove from host cluster.
+        candidates = []
+        for neighbor_cluster_index in neighbor_clusters:
+            candidate = copy.deepcopy(current_clustering)
+            candidate[neighbors_cluster_index].union(component)
+            if current_clustering[host_cluster_index] == component:
+                candidate.remove(component)
+            else:
+                candidate[host_cluster_index] -= component
+            candidates.append((candidate, cut_edges_dict[neighbors_cluster_index]))
+
+        # 2. As a new cluster
+        new_candidate = copy.deepcopy(current_clustering)
+        if current_clustering[host_cluster_index] == component:
+            new_candidate.append(new_candidate)
+        else:
+            new_candidate[host_cluster_index] -= component
+            new_candidate.append(component)
+        candidates.append(new_candidate, set())
+
         posteriors = []
         denominator = mpmath.mpf(0.0)
-        for label in candidate_labels:
-            labeling = copy.copy(current_labeling)
-            for v in component:
-                labeling[v] = label
-
+        for (candidate, cut_set) in candidates:
             # This weighted posterior guarantees the detailed balance.
             weight = mpmath.mpf(1.0)
-            for (s, t) in cut_edges_dict[label]:
+            for (s, t) in cut_set:
                 weight *= (1 - self.__edge_on_probability(s, t))
-            val = mpmath.mpf(self.target_eval_func(labeling, self.context))
+            val = mpmath.mpf(self.target_eval_func(candidate, self.context))
             posterior = weight * val
 
-            labeling_candidates.append(labeling)
             posteriors.append(posterior)
             denominator += posterior
 
@@ -305,16 +307,11 @@ class _SWCuts(object):
 
         print('Posteriors: {0}'.format(posteriors))
         print('Component: {0}'.format(component))
-        print('Candidate: {0}'.format(candidate_labels))
-        print('Previous Labeling: {0}'.format([current_labeling[v] for v in component]))
+        print('# of candidate: {0}'.format(len(candidates)))
 
         # Sample from the posterior probability.
-        selected_labeling = current_labeling
         cdf = [sum(posteriors[0:x]) for x in range(1, len(posteriors)+1)]
         r = random.random()
         for i in range(0, len(cdf)):
             if cdf[i] > r:
-                selected_labeling = labeling_candidates[i]
-                print('Selected Labeling: {0}'.format([selected_labeling[v] for v in component]))
-                break
-        return selected_labeling
+                return candidates[i]
