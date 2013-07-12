9575aa7c709c2d78954d1559102964dc9d702b20
Hang Qi <qih@qihang.im>
2013-07-11 17:13:23 -0700

    Add definition for distribution, node and tree. Add more skelton.

--- sw-clust/model/topic_model.py
+++ sw-clust/model/topic_model.py
@@ -2,34 +2,60 @@ import sys
 import datetime
 sys.path.append('..')
 
+import numpy as np
+import mpmath
+
 from algorithm import sw
 
 
 class _Plotter(object):
-    def plot_callback(self):
+    def __init__(self):
+        pass
+
+    def plot_callback(self, clustering, context):
+        # TODO: some drawings.
         pass
 
 
 class SWConfig(object):
     """One shall inherit this class to give more specific configurations."""
-    def __init__(self, graph_size, edges):
+    def __init__(self, graph_size, edges, level):
         self.graph_size = graph_size
         self.edges = edges
+        self.level = level
+        self.monitor_statistics = self.energy
 
-    def edge_prob_func(self):
+    def edge_prob_func(self, s, t, context):
+        # TODO: calculate base on KL divergence.
         return 0.5
 
-    def target_eval_func(self):
-        return 1
+    def target_eval_func(self, clustering, context=None):
+        temperature = self.cooling_schedule(context.iteration_counter)
+        return mpmath.exp(- self.energy() / temperature)
 
-    def monitor_statistics(self):
-        return 0.5
+    def energy(self, clustering):
+        energy = 0
+        # TODO: target function may depend on level.
+        # Candidate terms: likelihood, time prior, and etc.
+        #     energy += -mpmath.log(P)
+        return energy
+
+    def cooling_schedule(self, iteration_counter):
+        # TODO: Cooling schedule may depend on level.
+        starting_temperature = 1000
+        period = 2
+        step_size = 10
+
+        temperature = starting_temperature - int(iteration_counter/period)*step_size
+        if temperature <= 0:
+            temperature = 0.1
+        return temperature
 
 
 class TopicModel(object):
     def __init__(self):
         self.corpus = None
-        self.topic_tree = None
+        self.topic_tree = _Tree()
         self.date_range = None
         self.monitor = ModelMonitor()
         pass
@@ -51,7 +77,7 @@ class TopicModel(object):
 
     def __inference(self, new_document):
         """Inference and attach the new document to the current topic tree."""
-        # This shall base on the likelihood and depth prior.
+        # TODO: This shall base on the likelihood and depth prior.
         pass
 
     def __reform_by_multilevel_sw(self):
@@ -86,15 +112,16 @@ class TopicModel(object):
 
     def __generate_sw_configuration(self, current_clustering, level_counter):
         """Generate sw configuration for next run base on current clustering result."""
+        # TODO: give different configurations based on level.
         if current_clustering == [] or level_counter == 1:
             # Generate initial configuration
-            config = SWConfig(graph_size=5, edges=[(1, 2), (0, 3)])
+            config = SWConfig(graph_size=15, edges=[(1, 2), (0, 3)], level=level_counter)
 
         elif level_counter == 2:
-            config = SWConfig(graph_size=5, edges=[(1, 2), (0, 3)])
+            config = SWConfig(graph_size=10, edges=[(1, 2), (0, 3)], level=level_counter)
 
         elif level_counter == 3:
-            config = SWConfig(graph_size=5, edges=[(1, 2), (0, 3)])
+            config = SWConfig(graph_size=5, edges=[(1, 2), (0, 3)], level=level_counter)
 
         return config
 
@@ -106,6 +133,8 @@ class TopicModel(object):
 
     def __add_level_to_tree(self, current_clustering):
         """Add a level to tree."""
+        self.topic_tree.add_level_on_top(current_clustering)
+        pass
 
 
 class ModelMonitor():
@@ -116,3 +145,68 @@ class ModelMonitor():
     def update_reform_time(self):
         self.last_reform_date = datetime.datetime.now()
         self.reform_counter += 1
+
+
+class _Distribution(object):
+    """A 1D histogram (normalized to 1)."""
+    def __init__(self, hist, denominator):
+        self.hist = hist
+        self.denominator = denominator
+
+    def combine(self, other):
+        # Recover the histogram before normalization and add up two histograms
+        self.hist = self.hist * self.denominator + other.hist * other.denominator
+
+        # Re-normalize to 1
+        denominator = self.hist.sum()
+        if denominator != 0:
+            self.hist /= denominator
+        self.denominator = denominator
+
+    def synthesize(self, num_words):
+        """Synthesize a set of words from the distribution."""
+        indexes = np.argsort(self.hist)
+        top_words_id = indexes[0][::-1][0:min(num_words, np.shape(self.hist)[1])]
+        return top_words_id.tolist()
+
+    def __getitem__(self, word_id):
+        return self.hist[0, word_id]
+
+
+class _TreeNode(object):
+    def __init__(self):
+        self.__children = []
+        self.__vp_distribution = None
+        self.__np1_distribution = None
+        self.__np2_distribution = None
+
+    def add_child(self, node):
+        self.__childend.append(node)
+        # TODO: merge distribution after add a child
+        # What if add a node to a terminal node (document).
+
+    def get_child(self, index):
+        return self.__children[index]
+
+    def is_terminal(self):
+        return (len(self.__children) == 0)
+
+
+class _Tree(object):
+    """A Tree structure."""
+    def __init__(self):
+        self.__root = _TreeNode()
+
+    def add_to_root(self, node):
+        """Add a node to the root."""
+        self.__root.add_child(node)
+
+    def add_level_on_top(self, clustering):
+        """Add a new level on the top of the tree."""
+        # e.g. clustering = [{1,2,3}, {4,5}, {5,6,7}]
+        new_root = _TreeNode()
+        for cluster in clustering:
+            new_parent_node = _TreeNode()
+            for vertex_index in cluster:
+                new_parent_node.add_child(self.__root.get_child(vertex_index))
+        self.__root = new_root
