be9059d9fb7c8f8de75a0e9658a47586fd62c7e2
Hang Qi <qih@qihang.im>
2013-07-27 21:23:43 -0700

    Relocate python scripts for aligner.

--- transcript-aligner/deploy.sh
+++ transcript-aligner/deploy.sh
@@ -2,7 +2,9 @@
 
 # This will deploy transcript-aligenr to /usr/local/bin folder
 
+# Deploy shall be conducted with 'qihang' account to have the write permission to bin folder.
+
 echo Deploying transcript-aligner...
-cp *.py /usr/local/bin/transcript_aligner/
+cp scripts/*.py /usr/local/bin/transcript_aligner/
 cp bin/aligner /usr/local/bin/transcript_aligner/bin/
 echo Done.
