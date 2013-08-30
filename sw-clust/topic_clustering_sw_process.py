86f5de27f4e612b4bf3ea4a4782618f1a78d5ac5
lexiwzx <lexiwzx@gmail.com>
2013-08-30 08:39:14 -0700

    Changes for new triplet files.

--- sw-clust/topic_clustering_sw_process.py
+++ sw-clust/topic_clustering_sw_process.py
@@ -6,14 +6,14 @@ from preprocessing import readingfiles
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    input_triplet_files = glob.glob('data/triplet_files_new_test_set/2008*.txt')
+    input_triplet_files = glob.glob('data/transformed_triplet_files_080819-081015/2008*.txt')
     input_triplet_files.sort()
     logging.debug('Files {0}'.format(len(input_triplet_files)))
 
     # Read in the triplets files, including the triplets and the time of the story
     original_documents = []
     for triplet_file in input_triplet_files:
-        original_documents.append(readingfiles.read_triplet_file(triplet_file, use_ocr=True))
+        original_documents.append(readingfiles.read_triplet_file(triplet_file, use_ocr=False))
 
     # Train topic model
     model = topic_model.TopicModel(classifier_model_filename='preprocessing/model_segmenter.txt')
